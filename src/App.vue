6f51fdfd2d1c0ea56775b5024e1db8bd86615dfe
Alex Mwangi <alex@stl-horizon.com>
2022-06-14 22:18:10 +0300

    Added Global styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
-
     <the-header></the-header>
-
     <router-view></router-view>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
-
-
 export default {
     components : {
         TheHeader,
@@ -16,6 +12,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: "Roboto", sans-serif;
+}
+
+body {
+    margin: 0;
+}
 
 </style>
