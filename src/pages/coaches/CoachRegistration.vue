7334c07bff5f1808904f708e26dd46f71b5918ac
Alex Mwangi <alex@stl-horizon.com>
2022-06-16 15:46:44 +0300

    Listens to save-data event and fires saveData which dispatches data to registerCoach action; Redirects user to CoachesList screen using this..replace('/coaches')

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -2,7 +2,7 @@
       <section>
             <base-card>
                   <h2>Register as a coach now</h2>
-                  <coach-form></coach-form>
+                  <coach-form @save-data="saveData"></coach-form>
             </base-card>
       </section>
 </template>
@@ -14,7 +14,18 @@ export default {
 
       components : {
             CoachForm,
+      },    
+      methods : {
+
+            saveData(data){
+                  this.$store.dispatch('coaches/registerCoach',data)
+
+                  // Redirect to coaches
+                 // this.$router.push('/coaches')
+                  //Diff with .push - We can't go back to the page we came from thereafter
+                  //And we don't want to go back once the form is submitted
+                  this.$router.replace('/coaches')
+            }
       }
-   
 }
 </script>
