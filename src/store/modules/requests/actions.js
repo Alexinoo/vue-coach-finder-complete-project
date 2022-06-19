88e5f97ea87dd74772d5c94445f5d38870ef0ef1
Alex Mwangi <alex@stl-horizon.com>
2022-06-19 15:15:25 +0300

    Store sent requests to Firebase via http fetch(); Fetched Requests from firebase and commit to setRequests mutation

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,13 +1,57 @@
 export default {
 
-    contactCoach(context , payload){
+   async contactCoach(context , payload){
         const newRequest = {
-            id : new Date().toISOString(),
-            coachId : payload.coachId ,
             userEmail : payload.email,
             message : payload.message
         }
 
+        const response = await fetch(`https://vue-http-demo-97b72-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,{
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(newRequest)
+
+        })
+
+        const responseData = response.json();
+        console.log(responseData);
+
+        if(!response.ok){
+            const error = new Error(responseData.message || 'Failed to send request');
+            throw error;
+        }
+
+        newRequest.id = responseData.name
+        newRequest.coachId = payload.coachId       
+
         context.commit('addRequests' , newRequest )
+    } ,
+
+    async fetchRequests(context){
+
+        const coachId = context.rootGetters.getUserId
+
+      const response = await  fetch(`https://vue-http-demo-97b72-default-rtdb.firebaseio.com/requests/${coachId}.json`)
+
+      const responseData = response.json()
+
+      if(!response.ok){
+          const error = new Error(responseData.message || 'Failed to fetch requests!')
+          throw error
+      }
+      const requests = []
+
+      for(const key in responseData){
+            const request = {
+                id : key ,
+                coachId: coachId,
+                userEmail : responseData[key].userEmail,
+                message : responseData[key].message
+            }
+          requests.push(request)
+      }
+        context.commit('setRequests',requests)
     }
 }
