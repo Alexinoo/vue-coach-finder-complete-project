1d9eb3e0ba2b90ade8e0bc394c633febaec4a63f
Alex Mwangi <alex@stl-horizon.com>
2022-06-19 16:59:07 +0300

    Added Not Found page content

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,14 +1,11 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <base-card>
+            <h2>Page Not Found</h2>
+            <p>This page could not be found. Maybe check out all our
+                 <router-link to="/coaches">coaches</router-link>
+            </p>
+        </base-card>
+    </section>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-</style>
